c794961817bb85c57ef4c00b5bf4fbe97f5c6fe2
Botalfredo <68150357+Botalfredo@users.noreply.github.com>
2023-03-28 22:48:33 +0200

    Update test_mqtt_ha.ino

--- test_mqtt_ha.ino
+++ test_mqtt_ha.ino
@@ -1,13 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <PubSubClient.h>
-
-
+#define ouverture_pin 14
+#define ouverturepieton_pin 12
+#define fermeture_pin 13
 //****************** CONFIGURATION ******************
 #define wifi_ssid "Livebox-32C0"
 #define wifi_password "JcVVqjJrUrviKTMGaG"
-//#define wifi_ssid "Freebox-38B461"
-//#define wifi_password "cordi-transmutat9-dimidiis-famino"
+//#define wifi_ssid1 "Freebox-38B461"
+//#define wifi_password1 "cordi-transmutat9-dimidiis-famino"
 
 #define mqtt_server "83.205.178.99" // your HA IP, example 192.XX.XXX.XX
 #define mqtt_user "MQTT_BOT"
@@ -34,19 +35,42 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
-    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is active low on the ESP-01)
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+    Serial.println("ouverture pieton");
+    digitalWrite(ouverturepieton_pin, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    client.publish(temperature_topic, String(0).c_str(), true);
+    delay(500);
+    digitalWrite(ouverturepieton_pin, LOW); 
+    digitalWrite(LED_BUILTIN, HIGH); 
   }
-
+  else if ((char)payload[0] == '2') {
+    Serial.print("ouverture");
+    digitalWrite(ouverture_pin, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    client.publish(temperature_topic, String(0).c_str(), true);
+    delay(500);
+    digitalWrite(ouverture_pin, LOW); 
+    digitalWrite(LED_BUILTIN, HIGH); 
+  }
+  else if ((char)payload[0] == '3') {
+    Serial.print("fermeture");
+    digitalWrite(fermeture_pin, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
+    client.publish(temperature_topic, String(0).c_str(), true);
+    delay(500);
+    digitalWrite(fermeture_pin, LOW); 
+    digitalWrite(LED_BUILTIN, HIGH); 
+  }
+  
 }
 
 void setup() {
   Serial.begin(115200);
   setup_wifi();
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ouverturepieton_pin, OUTPUT);
+  pinMode(ouverture_pin, OUTPUT);
+  pinMode(fermeture_pin, OUTPUT);
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
